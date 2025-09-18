1f08f5f57eacf39fe95a890b7ba6ad26945c70c3
phanuel_isdev <phanuel.alibia@gmail.com>
2025-09-18 07:19:41 +0100

    testimonial and projet left

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,8 @@
 // app/_layout.tsx
 import { useUserStore } from "@/store/userStore";
-import { STRIPE_PUBLISHABLE_KEY } from "@env";
-import { StripeProvider } from "@stripe/stripe-react-native";
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
@@ -19,39 +18,31 @@ export default function RootLayout() {
   }, []);
 
   if (loading) {
-    return null; // 🔹 tu peux mettre un loader ici si tu veux
+    return <ActivityIndicator size="large" color="#fff" />;
   }
 
   return (
-    <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
-      <>
-        <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="pro" />
-          <Stack.Screen name="product" />
-          <Stack.Screen
-            name="enterprise-details"
-            options={{
-              title: "Détails de l'entreprise",
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen name="product-details/[id]" />
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="(profile-particulier)" />
-          <Stack.Screen name="(details-products)" />
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen
-            name="(professionnel)"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="(inventory)" />
-          <Stack.Screen name="(orders)" />
-          <Stack.Screen name="onboarding" />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <Toast />
-      </>
-    </StripeProvider>
+    <>
+      <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="pro" />
+        <Stack.Screen name="product" />
+        <Stack.Screen
+          name="enterprise-details"
+          options={{ title: "Détails de l'entreprise", headerShown: false }}
+        />
+        <Stack.Screen name="product-details/[id]" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(profile-particulier)" />
+        <Stack.Screen name="(details-products)" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(professionnel)" options={{ headerShown: false }} />
+        <Stack.Screen name="(inventory)" />
+        <Stack.Screen name="(orders)" />
+        <Stack.Screen name="onboarding" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <Toast />
+    </>
   );
 }
